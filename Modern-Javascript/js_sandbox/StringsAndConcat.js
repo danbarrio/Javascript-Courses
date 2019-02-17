ff82e73a3e779b7c2e5f7a4cea74315d4e51d99c
Dan Barrio <danbarrio@users.noreply.github.com>
2019-02-17 12:05:43 -0800

    Various String manipulation methods

--- Modern-Javascript/js_sandbox/StringsAndConcat.js
+++ Modern-Javascript/js_sandbox/StringsAndConcat.js
@@ -1,6 +1,8 @@
 const firstName = 'William';
 const lastName = 'Johnson';
 const age = 36;
+const str = 'Hello there my name is Brad';
+const tags = 'web design,web development,programming';
 
 let val;
 
@@ -14,5 +16,48 @@ val = 'Brad ';
 val += 'Traversy';
 
 val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// Escaping
+val = 'That\'s awesome, I can\'t wait';
+
+// Length
+val = firstName.length;
+
+// concat()
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = lastName.toLowerCase();
+
+val = firstName[2];
+
+// indexOf()
+val = firstName.indexOf('l');
+// from right to left
+val = firstName.lastIndexOf('l');
+
+// charAt()
+val = firstName.charAt(2);
+// get last char even if string changes
+val = firstName.charAt(firstName.length - 1)
+
+// substring()  pull a section from a string
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0, 4);
+// start from the end and get the last 3 characters
+val = firstName.slice(-3);
+
+// split   splits each item of string into an array element
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Brad', 'Jack');
+
+// includes()   checks whether string contains value and returns TRUE or FALSE
+val = str.includes('foo');
 
 console.log(val);
