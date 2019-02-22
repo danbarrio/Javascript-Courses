6700f7a0fa714512068e634b8c13fe2fd41a73fd
Dan Barrio <danbarrio@users.noreply.github.com>
2019-02-21 23:00:36 -0800

    If Statements pt1

---
+++ Modern-Javascript/js_sandbox/IfStatements.js
@@ -0,0 +1,42 @@
+// if(something){
+//   do something
+// } else {
+//   do something else
+// }
+
+//const id = 100;
+
+// EQUAL TO
+// if (id == 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if (id != 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE AND TYPE
+// if (id === 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO VALUE AND TYPE
+// if (id !== 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// Check if id exists
+if (typeof id !== 'undefined'){
+  console.log(`The ID is ${id}`);
+} else {
+  console.log('NO ID');
+}
