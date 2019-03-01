63e694e4bda983c0217ebbfd974fc142319cedce
Dan Barrio <danbarrio@users.noreply.github.com>
2019-03-01 09:04:20 -0800

    General Loops

---
+++ Modern-Javascript/js_sandbox/GeneralLoops.js
@@ -0,0 +1,36 @@
+// FOR LOOP
+
+// for(let i = 0; i < 10; i++){
+//   // console.log('Number ' + i);
+//   if(i === 2){
+//     console.log('2 is my favorite number')
+//     continue;
+//   }
+
+// if(i === 5){
+//   console.log('Stop the loop')
+//   break;
+// }
+
+//   console.log('Number ' + i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while(i < 10){
+//   console.log('Number ' + i)
+//   i++;
+// }
+
+// DO WHILE
+
+let i = 0;
+
+do {
+  console.log('Number ' + i)
+  i++;
+}
+
+while(i < 10);
