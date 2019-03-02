cd9294805c936345b41de33d56f6e33383a4d5ff
Dan Barrio <danbarrio@users.noreply.github.com>
2019-03-01 19:19:34 -0800

    General Loops pt 2

--- Modern-Javascript/js_sandbox/GeneralLoops.js
+++ Modern-Javascript/js_sandbox/GeneralLoops.js
@@ -26,11 +26,51 @@
 
 // DO WHILE
 
-let i = 0;
+// let i = 0;
 
-do {
-  console.log('Number ' + i)
-  i++;
+// do {
+//   console.log('Number ' + i)
+//   i++;
+// }
+
+// while(i < 10);
+
+// LOOP THROUGH ARRAY
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for(let i = 0; i < cars.length; i++){
+//   console.log(cars[i]);
+// }
+
+// FOREACH
+// cars.forEach(function(car, index, array){
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users = [
+//   {id: 1, name:'John'},
+//   {id: 2, name:'Sara'},
+//   {id: 3, name:'John'},
+//   {id: 4, name:'John'}
+// ];
+
+// const ids = users.map(function(user){
+//   return user.id;
+// });
+
+// console.log(ids);
+
+//FOR IN LOOP
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 40
 }
 
-while(i < 10);
+for(let x in user){
+  console.log(`${x} : ${user[x]}`);
+}
+
+
